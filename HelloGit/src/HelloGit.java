a7cb508e72323bc4173a7a16f9f705c6fef7a06b
bit <bit@DESKTOP-010721N>
2018-03-22 14:26:36 +0900

    2_add_hotfix_00

--- HelloGit/src/HelloGit.java
+++ HelloGit/src/HelloGit.java
@@ -3,8 +3,16 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		System.out.println("1_Hello_Git");
+		//	1. hotfix 추가 후 마스터 브랜치 수정
+		System.out.println("2_add_hotfix");
+		
+		
+		
 	}
 
 }
+
+
+
+

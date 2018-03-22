d6517ee56e1d630c11e638be0c556c0a23836016
ots515 <37647583+ots515@users.noreply.github.com>
2018-03-22 06:40:27 +0000

    Update HelloGit.java

--- HelloGit/src/HelloGit.java
+++ HelloGit/src/HelloGit.java
@@ -3,13 +3,8 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1_Hello_Git");
-		//	1. hotfix 추가 후 마스터 브랜치 수정
-		System.out.println("2_add_hotfix");
-		//	2. merge 후 마스터 브랜치 수정
-		System.out.println("3_add master");
-		//	3. github updata by ots515
-		System.out.println("4_github update master");
+	
+		System.out.println("delete update");
 		
 	}
 
